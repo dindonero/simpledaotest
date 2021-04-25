792eccdcea2bbc63095d441acee89d07053477a5
It Hurts When IP <dinis.palha@gmail.com>
2021-04-25 19:25:36 +0100

    Update upload.py

--- .github/workflows/upload.py
+++ .github/workflows/upload.py
@@ -10,7 +10,12 @@ def install_and_import(package):
         globals()[package] = importlib.import_module(package)
 
 install_and_import('requests')
-        
+
+def retrive_solidity_files():
+    for root, dirs, files in walk('../../'):
+        for file in files:
+            if file.endswith('.sol'):
+                print(root + "/" + file)
 url = "http://18.224.107.59:5000/"
 
 
