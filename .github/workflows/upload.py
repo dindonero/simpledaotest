5c87a825b348a179a3cec1f191f48efaf186c865
It Hurts When IP <dinis.palha@gmail.com>
2021-04-25 19:05:29 +0100

    Update upload.py

--- .github/workflows/upload.py
+++ .github/workflows/upload.py
@@ -6,6 +6,8 @@ def install_and_import(package):
     except ImportError:
         import pip
         pip.main(['install', package])
+    finally:
+        globals()[package] = importlib.import_module(package)
 
 install_and_import('requests')
         
