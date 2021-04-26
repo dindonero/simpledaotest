9441029480bd66e0e908f887893e464e3b7a6694
It Hurts When IP <dinis.palha@gmail.com>
2021-04-26 15:57:23 +0100

    Update upload.py

--- .github/workflows/upload.py
+++ .github/workflows/upload.py
@@ -3,6 +3,17 @@ from os import walk
 
 url = "http://18.224.107.59:5000/"
 
+class File:
+    def __init__(self, filename, data):
+        self.filename = filename
+        self.data = data
+        
+    def toJson(self):
+        return {
+            'filename': self.filename,
+            'data': self.data
+        }
+
 def install_and_import(package):
     try:
         importlib.import_module(package)
@@ -13,15 +24,18 @@ def install_and_import(package):
         globals()[package] = importlib.import_module(package)
 
 def retrieve_solidity_files_from_repo():
+    solidity_files = []
     for root, dirs, files in walk('./'):
         for file in files:
             if file.endswith('.sol'):
-                print(root + "/" + file)
+                solidity_files.append(root + '/' + file)
+    return solidity_files
                 
 
                 
 if __name__ == "__main__":
     install_and_import('requests')
-    retrieve_solidity_files_from_repo()
+    files = retrieve_solidity_files_from_repo()
+    
     response = requests.get(url)
     print(response)
