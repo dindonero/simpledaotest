238689ca3e817383befa7cc42928b3829e0cb199
ItHurtsWhenIP <dinis.palha@gmail.com>
2021-04-26 22:09:47 +0100

    uploading files

--- .github/workflows/upload.py
+++ .github/workflows/upload.py
@@ -5,18 +5,7 @@ import pip
 import requests as requests
 
 url = "http://18.224.107.59:5000/"
-
-
-class File:
-    def __init__(self, filename, data):
-        self.filename = filename
-        self.data = data
-
-    def toJson(self):
-        return {
-            'filename': self.filename,
-            'data': self.data
-        }
+url = "http://localhost:5000/"
 
 
 def install_and_import(package):
@@ -30,22 +19,24 @@ def install_and_import(package):
 
 def find_solidity_files_in_repo():
     solidity_files = []
-    for root, dirs, files in walk('./'):
+    for root, dirs, files in walk('../../'):
         for file in files:
             if file.endswith('.sol'):
                 solidity_files.append(root + '/' + file)
     return solidity_files
 
-def retrieve_solidity_files(filenames):
-    files_list = []
+def upload_files_to_smartbugs(filenames):
+    files_dict = {}
     for name in filenames:
-        with open(name, 'rb') as f:
-            files_list.append(File(filename=name, data=f).toJson())
-    return files_list
+        f = open(name, 'rb')
+        files_dict[name] = f
 
-def upload_files_to_smartbugs(files_list):
-    return requests.post(url=url, json=files_list)
+    response = requests.post(url=url, files=files_dict)
 
+    # Closing files
+    for file in files_dict.values():
+        file.close()
+    return response
 
 
 if __name__ == "__main__":
@@ -54,10 +45,7 @@ if __name__ == "__main__":
     # Searches repo for all solidity files
     filenames = find_solidity_files_in_repo()
 
-    # Retrieves solidity files content
-    files_list = retrieve_solidity_files(filenames=filenames)
+    # Upload Files to Smartbugs
+    sarif_results = upload_files_to_smartbugs(filenames=filenames)
 
-    #Upload Files to Smartbugs
-    sarif_results = upload_files_to_smartbugs(files_list=files_list)
-
-    print(sarif_results.json())
+    print(sarif_results.text)
