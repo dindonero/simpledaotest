6a73e10751753a0ba3e337a4d03a1e41c19adbc8
ItHurtsWhenIP <dinis.palha@gmail.com>
2021-05-04 18:20:35 +0100

    working

--- .github/workflows/upload.py
+++ .github/workflows/upload.py
@@ -1,51 +1,50 @@
-import importlib
 from os import walk
 
-import pip
-import requests as requests
+import requests
 
 url = "http://18.224.107.59:5000/"
 
 
-def install_and_import(package):
-    try:
-        importlib.import_module(package)
-    except ImportError:
-        pip.main(['install', package])
-    finally:
-        globals()[package] = importlib.import_module(package)
-
-
 def find_solidity_files_in_repo():
     solidity_files = []
-    for root, dirs, files in walk('./'):
+    for root, dirs, files in walk('../../'):
         for file in files:
             if file.endswith('.sol'):
                 solidity_files.append(root + '/' + file)
     return solidity_files
 
 
-def upload_files_to_smartbugs(filenames):
+def create_body(user_hash, tools):
+    return {
+        'user-hash': user_hash,
+        'tools': tools
+    }
+
+
+def upload_files_to_smartbugs(filenames, body):
     files_dict = {}
     for name in filenames:
         f = open(name, 'rb')
         files_dict[name] = f
 
-    response = requests.post(url=url, files=files_dict)
+    response = requests.post(url=url, data=body, files=files_dict)
 
-    # Closing files
+    # Close files
     for file in files_dict.values():
         file.close()
     return response
 
 
 if __name__ == "__main__":
-    install_and_import('requests')
-
     # Searches repo for all solidity files
     filenames = find_solidity_files_in_repo()
 
-    # Upload Files to Smartbugs
-    sarif_results = upload_files_to_smartbugs(filenames=filenames)
+    # Create Body Data
+    body = create_body(user_hash='0001testvalue', tools='oyente')
 
-    print(sarif_results.text)
+    # Upload Files to Smartbugs
+    sarif_results = upload_files_to_smartbugs(filenames=filenames, body=body)
+
+    # Save SARIF file on user repo
+    with open('../../.github/workflows/' + 'results.sarif', 'w') as sarif_file:
+        sarif_file.write(sarif_results.text)
